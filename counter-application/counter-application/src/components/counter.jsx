ba97d2292f003967f45daf45b55449646dde4345
Angel Cochachez <acochachez@gmail.com>
2020-03-24 14:24:43 -0700

    added counter.jsx and index.js updates

--- counter-application/counter-application/src/components/counter.jsx
+++ counter-application/counter-application/src/components/counter.jsx
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state={
-        count:0,
-        
-    };
-    
-    render() { 
-        
-        return(
-            <div>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
-            </div>
-        );
+	state = {
+		count: 1
+	};
+	styles = {
+		fontSize: 50,
+		fontWeight: 'bold'
+	};
+
+	render() {
+
+        let classes = this.newMethod();
+
+		return (
+			<div>
+				<span className={classes}>{this.formatCount()}</span>
+				
+				<button className='btn btn-secondary btn-sm'>Increment</button>
+			</div>
+		);
+	}
+    newMethod() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
-    formatCount(){
-        const{count}=this.state;
-        
-        return count === 0 ? 'Zero': count;
-    }
+
+	formatCount() {
+		const { count } = this.state;
+
+		return count === 0 ? 'Zero' : count;
+	}
 }
 
- 
 export default Counter;
